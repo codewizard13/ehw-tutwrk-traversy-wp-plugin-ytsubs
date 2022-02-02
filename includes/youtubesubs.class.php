3971341c5638ad1478231f3afc2a2ec030b89f42
Eric L. Hepperle <codewizard13@gmail.com>
2022-02-02 11:54:29 -0600

    INITIAL COMMIT

---
+++ includes/youtubesubs.class.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Adds Foo_Widget widget.
+ */
+class Youtube_Subs_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'youtubesubs_widget', // Base ID
+			esc_html__( 'YouTube Subs', 'yts_domain' ), // Name
+			array( 'description' => esc_html__( 'Widget to display YouTube subs', 'yts_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget']; // Whatever you want to display BEFORE widget (<div>, etc)
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+		}
+
+        // Widget Content Output
+		// echo esc_html__( 'Hello, World!', 'yts_domain' );
+        echo 'Hello from YTS Widget!';
+
+		echo $args['after_widget']; // Whatever you want to display AFTER widget (<div>, etc)
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'YouTube Subs', 'yts_domain' );
+		?>
+
+        <!-- TITLE -->
+		<p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>">
+                <?php esc_attr_e( 'Title:', 'yts_domain' ); ?>
+            </label> 
+            <input 
+                class="widefat"
+                id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" 
+                name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" 
+                type="text" 
+                value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+
+} // class Foo_Widget
---
+++ youtubesubs.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: YouTube Subs
+Plugin URI: https://erichepperle.com
+Description: Display YouTube sub button and count
+Version: 1.1.0
+Author: Eric Hepperle
+Author URI: https://erichepperle.com
+Date Created: 02/02/22
+*/
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+   exit;
+}
+
+// Load Scripts
+require_once(plugin_dir_path(__FILE__).'/includes/youtubesubs-scripts.php');
+
+// Load Class
+require_once(plugin_dir_path(__FILE__).'/includes/youtubesubs-class.php');
+
+// Register Widget
+function register_youtubesubs() {
+    register_widget('YouTube_Subs_Widget');
+}
+
+// Hook in function
+add_action ('widgets_init', 'register_youtubesubs');
